d6e921c086e0b96f06c0c64b055f2a064e30be84
ccficorilli <ccficorilli@gmail.com>
2018-05-09 13:37:07 -0700

    updating alts for imgs

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,7 +8,7 @@ class NavBar extends Component {
         if(this.props.dropdown === true){
             return(
                 <div className="navbar">
-                    <img className="menuTab" src={menu} onClick={this.props.toggleDropdown}/>
+                    <img className="menuTab" src={menu} onClick={this.props.toggleDropdown} alt="broken image"/>
                             <button className="nav-btn reset-btn" onClick={this.props.reset}>Reset</button>
                             <button className="nav-btn burner">Exercise</button>
                             <button className="nav-btn nearby">Markets</button>
@@ -17,7 +17,7 @@ class NavBar extends Component {
                 </div>
             )}else return(
                 <div className="navbar">
-                    <img className="menuTab" src={menu} onClick={this.props.toggleDropdown}/>
+                    <img className="menuTab" src={menu} onClick={this.props.toggleDropdown} alt="broken image"/>
                 </div>
             )}
 }
